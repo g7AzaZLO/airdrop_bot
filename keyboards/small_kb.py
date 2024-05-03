c817dd3069bc96fbe452c70432d5dd416079523f
Maloy <MaloyMeee@yandex.ru>
2024-05-04 01:30:38 +0300

    add keyboard file

---
+++ keyboards/small_kb.py
@@ -0,0 +1,8 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+join_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        KeyboardButton(text="🚀 Join Airdrop"),
+    ],
+    resize_keyboard=True,
+)
