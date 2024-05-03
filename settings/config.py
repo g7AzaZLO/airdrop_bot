57ebb39bf062ff6f980237ce7e485aa5e62b8c2d
Maloy <MaloyMeee@yandex.ru>
2024-05-04 01:02:11 +0300

    init

---
+++ settings/config.py
@@ -0,0 +1,21 @@
+import os
+
+# General
+COIN_SYMBOL = "TIME"
+COIN_NAME = "Buy or Die"
+AIRDROP_TOTAL = 50000000
+AIRDROP_NETWORK = "TON"
+REFERRAL_REWARD = 1000
+
+# Links
+TWITTER_LINKS = ["https://twitter.com/buyordie_ton"]
+TELEGRAM_LINKS = ["https://t.me/buyordie_ton", "https://t.me/buyordie_chat"]
+WEBSITE_URL = "https://buyordie.fun/"
+MAX_USERS = 100000
+MAX_REFS = 100
+
+# Admins
+ADMIN_USERNAME = "\n".join(os.environ["ADMIN_USERNAME"].split(","))
+
+# Token
+BOT_TOKEN = os.environ["BOT_TOKEN"]
