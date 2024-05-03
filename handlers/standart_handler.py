4f6085b0d16317f44ec3d96a01e9027f359b6b81
Maloy <MaloyMeee@yandex.ru>
2024-05-04 01:30:49 +0300

    add handler file

---
+++ handlers/standart_handler.py
@@ -0,0 +1,12 @@
+import asyncio
+from aiogram import types, Router
+from aiogram.filters import CommandStart
+from settings.static_messages import WELCOME_MESSAGE
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def start(message: types.Message) -> None:
+    print("Processing /start command...")
+    await message.answer(text=WELCOME_MESSAGE)
