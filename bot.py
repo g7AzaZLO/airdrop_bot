f4d5bdfb2cd3182403302ec84923b39518de728b
Maloy <MaloyMeee@yandex.ru>
2024-05-04 02:03:22 +0300

    edit comments

--- bot.py
+++ bot.py
@@ -3,14 +3,14 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from settings.config import BOT_TOKEN
 
-# Bot initial
+# Инициализация бота
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
 async def main() -> None:
-    await bot.delete_webhook(drop_pending_updates=True)  # Skip message when bot offline
+    await bot.delete_webhook(drop_pending_updates=True)  # Пропускает сообщения накопившиеся пока бот отключен
     await dp.start_polling(bot)
 
 
--- handlers/standart_handler.py
+++ handlers/standart_handler.py
@@ -1,4 +1,3 @@
-import asyncio
 from aiogram import types, Router
 from aiogram.filters import CommandStart
 from settings.static_messages import WELCOME_MESSAGE
