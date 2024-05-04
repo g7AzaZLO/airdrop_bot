1712a0e0367371b63ba0adc30a20c125e7f7256d
Maloy <MaloyMeee@yandex.ru>
2024-05-04 14:38:59 +0300

    add states

---
+++ FSM/states.py
@@ -0,0 +1,13 @@
+from aiogram.fsm.state import StatesGroup, State
+
+
+class CaptchaState(StatesGroup):
+    wait_captcha_state = State()
+
+class RegestrationState(StatesGroup):
+    proceed_state = State()
+    follow_telegram_state = State()
+    follow_twitter_state = State()
+    submit_address_state = State()
+    end_conversation_state = State()
+    loop_state = State()
