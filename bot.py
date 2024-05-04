e84b29828f1672d20c628b116eddfa099697fd06
Maloy <MaloyMeee@yandex.ru>
2024-05-04 14:40:48 +0300

    include router

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from settings.config import BOT_TOKEN
+from handlers.standart_handler import standart_handler_router
 
 # import os
 # Инициализация бота
@@ -9,6 +10,7 @@ from settings.config import BOT_TOKEN
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+dp.include_router(standart_handler_router)
 
 async def main() -> None:
 	await bot.delete_webhook(drop_pending_updates=True)  # Пропускает сообщения накопившиеся пока бот отключен
