9846e432d45d4e6289652491209a1d8d740ea0ea
Maloy <MaloyMeee@yandex.ru>
2024-05-06 23:30:46 +0300

    add print log

--- logic/refs.py
+++ logic/refs.py
@@ -5,6 +5,7 @@ async def get_refferal_link(user_id: int) -> str:
     """
     Функция возвращает ссылку для реферальной программы.
     """
+    print(f"https://t.me/{BOT_NICKNAME}?start={user_id}")
     return f"https://t.me/{BOT_NICKNAME}?start={user_id}"
 
 
@@ -12,4 +13,5 @@ async def get_refferer_id(link: str) -> str:
     """"
     Функция возвращает id реферера из ссылки.
     """
+    print(link[7:])
     return link[7:]
