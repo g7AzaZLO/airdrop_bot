5a83454f9c209433e37eb8f6df3cf59f44924111
Maloy <MaloyMeee@yandex.ru>
2024-05-12 13:49:01 +0300

    def return_task_info

--- logic/task.py
+++ logic/task.py
@@ -1,4 +1,7 @@
+from aiogram import types, Router
 from tasks.task_dict import tasks, task_types
+
+task_router = Router()
 
 
 async def get_all_points() -> int:
@@ -12,3 +15,13 @@ async def get_all_points() -> int:
 async def get_num_of_tasks() -> int:
     print("def get_num_of_tasks")
     return len(tasks)
+
+
+async def return_task_info(message: types.Message, task: str) -> None:
+    print("def return_task_info")
+    info = tasks[task]
+    photo = info["image"]
+    description = info["description"]
+    points = info["points"]
+    await message.answer_photo(photo)
+    await message.answer(text=description)
