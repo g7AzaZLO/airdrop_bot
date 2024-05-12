3034fc1eb47f8f5d51d3816876ad959ab8a5b35c
Maloy <MaloyMeee@yandex.ru>
2024-05-12 15:19:11 +0300

    delete task_types dict

--- tasks/task_dict.py
+++ tasks/task_dict.py
@@ -5,37 +5,30 @@ tasks = {
         "image": "task1.png",
         "description": "Подпишитесь на наш канал Telegram",
         "points": 50,
-        "type": "telegram_sub"
+        "type": "telegram_sub",
+        "protection": None
     },
     "task2": {
         "image": "task2.png",
         "description": "Сделайте репост нашего поста в Twitter",
         "points": 100,
-        "type": "twitter_retweet"
+        "type": "twitter_retweet",
+        "protection": "bot_check"
     },
     "task3": {
         "image": "task3.png",
         "description": "Оставьте комментарий в Twitter",
         "points": 150,
-        "type": "twitter_comment"
+        "type": "twitter_comment",
+        "protection": "screen_check"
     },
     "task4": {
         "image": "task4.png",
         "description": "Оставьте комментарий в Telegram",
         "points": 200,
-        "type": "telegram_comment"
-    }
-}
-
-# Словарь типов задач
-task_types = {
-    "telegram_sub": {
-        "protection": "bot_check"
-    },
-    "twitter_retweet": {
-        "protection": None
-    },
-    "twitter_comment": {
+        "type": "telegram_comment",
         "protection": "screen_check"
     }
 }
+
+protection_fot_admins = ["screen_check",]
